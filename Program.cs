74fc4fbda44555430c2e9b5cbe288083f69e9827
Pedro Lucas Kawano <pedrolucas_kawano@hotmail.com>
2023-09-09 16:52:21 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=sa;Password=SQLServer2016;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            ReadUsers(connection);
+            ReadRoles(connection);
+            ReadTags(connection);
+
+            connection.Close();
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+    }
+}
